e7101760f8a636cc16bb58fa6796dd2bac095b39
Dikdik Firman Sidik <dikdikalihsan@gmail.com>
2021-11-09 10:09:50 +0700

    menambahkan fungsi dari sign in ke forgot password

--- app/src/main/java/com/rsudalihsan/attendanceapplication/SignInActivity.kt
+++ app/src/main/java/com/rsudalihsan/attendanceapplication/SignInActivity.kt
@@ -3,11 +3,18 @@ package com.rsudalihsan.attendanceapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val btnForgotPassword : TextView = findViewById(R.id.btnForgotPassword)
+        btnForgotPassword.setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
+        }
 
 
     }
