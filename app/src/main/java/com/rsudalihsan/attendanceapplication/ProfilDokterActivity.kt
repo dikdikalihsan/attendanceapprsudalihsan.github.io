66b462cf1f17adf90f978ed14238f5c023d76942
Dikdik Firman Sidik <dikdikalihsan@gmail.com>
2021-11-12 11:03:39 +0700

    menambahkan profil dokter

---
+++ app/src/main/java/com/rsudalihsan/attendanceapplication/ProfilDokterActivity.kt
@@ -0,0 +1,11 @@
+package com.rsudalihsan.attendanceapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilDokterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_dokter)
+    }
+}
