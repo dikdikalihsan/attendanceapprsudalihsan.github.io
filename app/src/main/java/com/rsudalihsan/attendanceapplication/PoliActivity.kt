6146a9a8455ebce2d618560edc2e43259987d7f6
Dikdik Firman Sidik <dikdikalihsan@gmail.com>
2021-11-16 13:57:31 +0700

    memperbaiki profil dokter

--- app/src/main/java/com/rsudalihsan/attendanceapplication/PoliActivity.kt
+++ app/src/main/java/com/rsudalihsan/attendanceapplication/PoliActivity.kt
@@ -1,11 +1,21 @@
 package com.rsudalihsan.attendanceapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class PoliActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poli)
+
+        val btnPoliDokterUmum = findViewById<FrameLayout>(R.id.btnPoliDokterUmum)
+        btnPoliDokterUmum.setOnClickListener {
+            startActivity(Intent(
+                this,
+                MainActivity::class.java
+            ))
+        }
     }
 }
